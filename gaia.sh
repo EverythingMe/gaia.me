0d3164873fa6d647ecacb5c55ee35bf864cf46b1
Amir Nissim <amirnissim@gmail.com>
2014-07-09 18:08:46 +0300

    fflog: show all gecko logs

--- gaia.sh
+++ gaia.sh
@@ -63,7 +63,7 @@ alias ffboot='adb shell reboot'
 alias fffwd='adb forward tcp:6000 localfilesystem:/data/local/debugger-socket'
 
 # logcat with Everything.me highlights
-alias fflogme='adb logcat | grep -E -i "xxx|error|query|feature|session|nativeinfo|evme|rocketbar"'
+alias fflog='adb logcat | grep E/GeckoConsole'
 
 # erase wifi data and reboot
 alias ffwipefy='adb shell rm -r data/misc/wifi && adb shell reboot'
